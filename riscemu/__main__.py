8d39d79032ad3ddfc81bb21b48325fa25995aba7
Anton Lydike <me@antonlydike.de>
2021-04-18 00:43:39 +0200

    made module runnable

--- riscemu/__main__.py
+++ riscemu/__main__.py
@@ -3,24 +3,27 @@ if __name__ == '__main__':
     from .helpers import *
     import argparse
 
-    parser = argparse.ArgumentParser(description='RISC-V Userspace parser and emulator')
+    parser = argparse.ArgumentParser(description='RISC-V Userspace parser and emulator', prog='riscemu')
     parser.add_argument('file', metavar='file.asm', type=str, help='The assembly file to interpret and run')
 
     # RunConfig parameters
-    parser.add_argument('color', type=bool, help='Colored output', default=True)
-    parser.add_argument('default_stack_size', type=int, help='Default stack size of loaded programs', default=None,
-                        metavar='default-stack-size')
-    parser.add_argument('debug_instruction', type=bool, default=True, metavar='debug-instruction',
+    parser.add_argument('--no-color', type=bool, help='no colored output', default=False,
+                        nargs='?')
+    parser.add_argument('--default_stack_size', type=int, help='Default stack size of loaded programs', default=None,
+                        metavar='default-stack-size', nargs='?')
+    parser.add_argument('--debug_instruction', type=bool, default=True, metavar='debug-instruction',
                         help='Switches to an interactive python interpreter when ebreak/sbreak instruction '
-                             'is encountered. Otherwise these instructions are treated as nop.')
+                             'is encountered. Otherwise these instructions are treated as nop.', nargs='?')
 
-    parser.add_argument('print_tokens', metavar='print-tokens', type=bool, help='Print tokens after tokenization',
-                        default=False)
+    parser.add_argument('--print_tokens', metavar='print-tokens', type=bool, help='Print tokens after tokenization',
+                        default=False, nargs='?')
 
     args = parser.parse_args()
 
+    print(args)
+
     cfg = RunConfig(
-        color=args.color,
+        color=not args.no_color,
         preffered_stack_size=args.default_stack_size,
         debug_instruction=args.debug_instruction
     )
