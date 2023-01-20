f7e7c4103468166fe52173e087984aa9e9dbf86b
Emilien Bauer <papychacal@gmail.com>
2023-01-20 14:13:53 +0000

    Deploy RiscEmu in a JupyterLite distribution. (#18)
    * Try and deploy a RiscEmu JupyterLite distribution.
    
    * Add examples to the JupyterLite deployment.
    
    * Add some demo notebooks and link to it in Readme.
    
    Co-authored-by: Emilien Bauer <bauer.emilien@gmail.com>

--- examples/fibs.asm
+++ examples/fibs.asm
@@ -10,7 +10,6 @@ main:
         addi    s2, zero, 56    ; last storage index
         addi    t0, zero, 1     ; t0 = F_{i}
         addi    t1, zero, 1     ; t1 = F_{i+1}
-        ebreak                  ; launch debugger
 loop:
         sw      t0, fibs(s1)    ; save
         add     t2, t1, t0      ; t2 = F_{i+2}
@@ -19,7 +18,6 @@ loop:
         addi    s1, s1, 4       ; increment storage pointer
         blt     s1, s2, loop    ; loop as long as we did not reach array length
         ; exit gracefully
-        ebreak                  ; launch debugger
         addi    a0, zero, 0
         addi    a7, zero, 93
         scall                   ; exit with code 0
