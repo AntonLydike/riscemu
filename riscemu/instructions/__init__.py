d3643f3b5904295bf98f46a31de6faa98dddb149
Anton Lydike <me@antonlydike.de>
2023-06-05 23:11:04 +0100

    minor: fix mising dot in instruction imort

--- riscemu/instructions/__init__.py
+++ riscemu/instructions/__init__.py
@@ -10,7 +10,7 @@ from .instruction_set import InstructionSet, Instruction
 from .RV32M import RV32M
 from .RV32I import RV32I
 from .RV32A import RV32A
-from RV32F import RV32F
+from .RV32F import RV32F
 from .RV_Debug import RV_Debug
 
 InstructionSetDict = {v.__name__: v for v in [RV32I, RV32M, RV32A, RV32F, RV_Debug]}
