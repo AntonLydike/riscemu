c2995b45d5d0296aa556fee0628cb7c5e0b9df0d
Alban Dutilleul <alban.dutilleul@ens-rennes.fr>
2023-08-21 14:21:39 +0100

    add a filecheck for float conversions

---
+++ test/filecheck/rv32f-conv.asm
@@ -0,0 +1,49 @@
+// RUN: python3 -m riscemu -v %s | filecheck %s
+
+.text
+main:
+    // test fcvt.s.wu
+    li a1, -2
+    fcvt.s.wu fa0, a1
+    print.float fa0
+    // CHECK: register fa0 contains value 4294967296.0
+    li a1, 2
+    fcvt.s.wu fa0, a1
+    print.float fa0
+    // CHECK-NEXT: register fa0 contains value 2.0
+
+    // test fcvt.s.w
+    li a1, -2
+    fcvt.s.w fa0, a1
+    print.float fa0
+    // CHECK: register fa0 contains value -2.0
+    li a1, 2
+    fcvt.s.w fa0, a1
+    print.float fa0
+    // CHECK-NEXT: register fa0 contains value 2.0
+
+    // test fmv.s.x 
+    li a1, 2
+    fcvt.s.w fa0, a1
+    fmv.x.w a1, fa0
+    print a1
+    // CHECK-NEXT: register a1 contains value 1073741824
+    li a1, -2
+    fcvt.s.w fa0, a1
+    fmv.x.w a1, fa0
+    print a1
+    // CHECK-NEXT: register a1 contains value 3221225472
+
+    // test fmv.w.x
+    li a1, 1073741824
+    fmv.w.x fa0, a1
+    print.float fa0
+    // CHECK-NEXT: register fa0 contains value 2.0
+    li a1, 3221225472
+    fmv.w.x fa0, a1
+    print.float fa0
+    // CHECK-NEXT: register fa0 contains value -2.0
+
+    addi    a0, zero, 0             // set exit code to 0
+    addi    a7, zero, SCALL_EXIT    // exit syscall code
+    scall
