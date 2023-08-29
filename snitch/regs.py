4485b057415d0d5291eb0721d0851d6c262a3444
KGrykiel <krzgrykiel@gmail.com>
2023-08-29 14:13:16 +0100

    Added streaming register bank and basic xssr instructions

---
+++ snitch/regs.py
@@ -0,0 +1,99 @@
+from riscemu.registers import Registers
+from riscemu.MMU import MMU
+from riscemu.types import Int32, UInt32
+
+from dataclasses import dataclass
+
+from enum import Enum
+
+from riscemu.types.float32 import Float32
+
+
+class StreamMode(Enum):
+    READ = 1
+    WRITE = 2
+    #READWRITE = 3
+
+@dataclass
+class StreamDef:
+    base: int
+    """
+    Base address to read from
+    """
+    bound: int
+    """
+    How many elements are in here
+    """
+    stride: int
+    """
+    stride = register width
+    """
+    mode: StreamMode = StreamMode.READ
+    """
+    Differentiate between read/write
+    """
+    dim: int
+    """
+    Supports nested loops
+    """
+    # internal:
+    pos: int = 0
+    """
+    Next element in stream
+    """
+
+
+class StreamingRegs(Registers):
+    mem: MMU
+    dm_by_id: list[StreamDef]
+    streams: dict[str, StreamDef]
+    enabled: bool
+
+    def __init__(self, mem: MMU, xssr_regs: tuple[str] = ('ft0', 'ft1', 'ft2'), infinite_regs: bool = False):
+        self.mem = mem
+        self.enabled = False
+        self.streams = dict()
+        self.dm_by_id = []
+        for reg in xssr_regs:
+            stream_def = StreamDef()
+            self.dm_by_id.append(stream_def)
+            self.streams[reg] = stream_def
+        super().__init__(infinite_regs)
+
+    def get_f(self, reg, mark_read=True) -> 'Float32':
+        if not self.enabled or reg not in self.streams:
+            return super().get_f(reg, mark_read)
+
+        # do the streaming stuff:
+        stream = self.streams[reg]
+        # TODO: Implement other modes
+        assert stream.mode is StreamMode.READ
+        # TODO: Check overflow
+        # TODO: repetition
+        addr = stream.base + (4 * stream.pos * stream.stride)
+        val = self.mem.read_float(addr)
+        # increment pos
+        print("stream: got val {} from addr 0x{:x}, stream {}".format(
+            val, addr, stream
+        ))
+        stream.pos += 1
+        return val
+
+    def set_f(self, reg, val: 'Float32', mark_set=True) -> bool:
+        if not self.enabled or reg not in self.streams:
+            return super().set_f(reg, mark_set)
+        
+        stream = self.streams[reg]
+        assert stream.mode is StreamMode.WRITE
+
+        addr = stream.base + (4 * stream.pos * stream.stride)
+        self.mem.write(addr, 4, bytearray(val.bytes))
+
+        print("stream: wrote val {} into addr 0x{:x}, stream {}".format(
+            val, addr, stream
+        ))
+        stream.pos += 1
+        return True
+
+
+
---
+++ snitch/xssr.py
@@ -0,0 +1,41 @@
+from riscemu.instructions.instruction_set import InstructionSet, Instruction
+
+from .regs import StreamingRegs, StreamDef, StreamMode
+
+
+class RV32_Xssr_pseudo(InstructionSet):
+    def instruction_ssr_enable(self, ins: Instruction):
+        self._stream.enabled = True
+
+    def instruction_ssr_disable(self, ins: Instruction):
+        self._stream.enabled = False
+    
+    def instruction_ssr_configure(self, ins: Instruction):
+        dm = ins.get_imm(0).abs_value
+        bound = ins.get_imm(1).abs_value
+        stride = ins.get_imm(2).abs_value
+
+        self._stream.dm_by_id[dm].bound = bound
+        self._stream.dm_by_id[dm].stride = stride
+
+    def instruction_ssr_read(self, ins: Instruction):
+        base_pointer = ins.get_reg(0)
+        dm = ins.get_imm(1).abs_value
+        dim = ins.get_imm(2).abs_value
+
+        self._stream.dm_by_id[dm].base = self.regs.get(base_pointer)
+        self._stream.dm_by_id[dm].dim = dim
+        self._stream.dm_by_id[dm].mode = StreamMode.READ
+
+    def instruction_ssr_write(self, ins: Instruction):
+        base_pointer = ins.get_reg(0)
+        dm = ins.get_imm(1).abs_value
+        dim = ins.get_imm(2).abs_value
+
+        self._stream.dm_by_id[dm].base = self.regs.get(base_pointer)
+        self._stream.dm_by_id[dm].dim = dim
+        self._stream.dm_by_id[dm].mode = StreamMode.WRITE
+
+    @property
+    def _stream(self) -> StreamingRegs:
+        return self.cpu.regs  #type: ignore
