7bc13eedc05cc436f26122425bfd8087b119101f
Joshy <janez.rotman@tum.de>
2023-10-05 15:45:53 +0200

    implement mulhsu and mulhu

--- riscemu/instructions/RV32M.py
+++ riscemu/instructions/RV32M.py
@@ -5,7 +5,6 @@ SPDX-License-Identifier: MIT
 """
 
 from .instruction_set import *
-from riscemu.types.exceptions import INS_NOT_IMPLEMENTED
 
 
 class RV32M(InstructionSet):
@@ -22,10 +21,12 @@ class RV32M(InstructionSet):
         self.regs.set(rd, Int32((rs1.signed().value * rs2.signed().value) >> 32))
 
     def instruction_mulhsu(self, ins: "Instruction"):
-        INS_NOT_IMPLEMENTED(ins)
+        rd, rs1, rs2 = self.parse_rd_rs_rs(ins)
+        self.regs.set(rd, Int32((rs1.signed().value * rs2.unsigned_value) >> 32))
 
     def instruction_mulhu(self, ins: "Instruction"):
-        INS_NOT_IMPLEMENTED(ins)
+        rd, rs1, rs2 = self.parse_rd_rs_rs(ins)
+        self.regs.set(rd, UInt32((rs1.unsigned_value * rs2.unsigned_value) >> 32))
 
     def instruction_div(self, ins: "Instruction"):
         rd, rs1, rs2 = self.parse_rd_rs_rs(ins)
