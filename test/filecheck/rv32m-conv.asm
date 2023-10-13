96e8680bd55c9f027139bd9698f89b1b1a14bc70
Joshy <janez.rotman@tum.de>
2023-10-13 14:51:03 +0200

    add filecheck test

---
+++ test/filecheck/rv32m-conv.asm
@@ -0,0 +1,37 @@
+// RUN: python3 -m riscemu -v %s -o libc | filecheck %s
+
+.text
+
+.globl      main
+main:
+    // test mulh
+    li a1, -1
+    mulh a0, a1, a1
+    print a0
+// CHECK: register a0 contains value 0
+    li a2, 2
+    mulh a0, a1, a2
+    print.hex a0
+// CHECK-NEXT: register a0 contains value 0xffffffff
+
+    // test mulhu
+    mulhu a0, a1, a2
+    print a0
+// CHECK: register a0 contains value 1
+
+    mulhu a0, a1, a1
+    print.hex a0
+// CHECK-NEXT: register a0 contains value 0xfffffffe
+
+    // test mulhsu
+    mulhsu a0, a1, a2
+    print.hex a0
+// CHECK: register a0 contains value 0xffffffff
+
+    mulhsu a0, a2, a1
+    print a0
+// CHECK-NEXT: register a0 contains value 1
+
+    li a0, 0
+    ret
+// CHECK-NEXT: [CPU] Program exited with code 0
